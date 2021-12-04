2e758b371bbede29b735a215ebea8dd4a0280dbf
LP-SAKTHI <reputation.3.0.1.1@gmail.com>
2021-12-04 14:58:12 +0530

    2nd commit

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
   // console.log(hisherName)
 
   const splitArray = (yourName,hisherName) => {
-    if(yourName === "" || hisherName === "")
+    if(yourName === null || hisherName === null)
     {
       console.log("Enter Both Names!")
     }
@@ -19,35 +19,67 @@ function App() {
       const yourNameSplit = yourName.split("")
       const hisherNameSplit = hisherName.split("")
       return [yourNameSplit,hisherNameSplit]
-      //console.log(yourNameSplit,hisherNameSplit);
-    
+      
     }
+    
   }
 
-    // const getRemainder = (splitname,name) =>{
+    const getRemainder = (splitname,name) =>{
 
-    //   splitname.map(item =>{
-    //     const regex = new RegExp(`${item}`,"i")
-    //     name = name.replace(regex,"")
+      splitname.map(item =>{
+        const regex = new RegExp(`${item}`,"i")
+        name = name.replace(regex,"")
         
-    //   })
-    //   console.log(name)
-    //   return [splitname,name]
-    // }
+      })
+      //console.log(name)
+      return [splitname,name]
+    }
 
 
+  const getCount = (yourRemainder,hisherRemainder) =>{
+
+    const totalwords = yourRemainder + hisherRemainder;
+    // console.log(totalwords);
+    const totalCount = totalwords.length;
+    //console.log(totalCount)
+  }
   
+  const getResult = (result,setCount) =>
+  {
+    if(yourName === true && hisherName === true)
+    {
+      for(let i = 6 ; i > 1 ; i--)
+    {
+      result.splice((setCount)%i,1)
+      console.log(result)
+      console.log((setCount),result.length)
+
+      if((setCount-1)%i < result.length || (setCount !== 0) )
+      {
+        let resultPart = result.splice((setCount)%i,)
+        result = [...resultPart,...result]
+        console.log(result)
+      }
+    }
+    }
+    
+  }
 
   
     const result = ['Friend','Lover','Affectionate','Marriage','Enemy','Sibling']
     //console.log(result)
 
     const splitnames = splitArray(yourName,hisherName) 
-    console.log(splitnames);
+    //console.log(splitnames);
     
 
-    // const yourRemainder = getRemainder(splitnames,yourName)
+    const yourRemainder = getRemainder(splitnames[1],yourName)
+    const hisherRemainder = getRemainder(splitnames[0],hisherName)
     // console.log(yourRemainder)
+    // console.log(hisherRemainder)
+    const setCount = getCount(yourRemainder[1],hisherRemainder[1])
+    const setResult = getResult(result,setCount)
+        // console.log(setResult)
 
     //console.log(splitnames[1]);
     
