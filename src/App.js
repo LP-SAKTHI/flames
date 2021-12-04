153e18d31947b8f8d03eb0024649e2947faf226e
LP-SAKTHI <reputation.3.0.1.1@gmail.com>
2021-12-04 12:54:24 +0530

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react/cjs/react.development"
+import Design from "./components/Design"
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+  const [yourName,setYourName] = useState("")
+  const [hisherName,sethisherName] = useState("")
+
+  // console.log(yourName)
+  // console.log(hisherName)
+
+  const splitArray = (yourName,hisherName) => {
+    if(yourName === "" || hisherName === "")
+    {
+      console.log("Enter Both Names!")
+    }
+    else{
+      const yourNameSplit = yourName.split("")
+      const hisherNameSplit = hisherName.split("")
+      return [yourNameSplit,hisherNameSplit]
+      //console.log(yourNameSplit,hisherNameSplit);
+    
+    }
+  }
+
+    // const getRemainder = (splitname,name) =>{
+
+    //   splitname.map(item =>{
+    //     const regex = new RegExp(`${item}`,"i")
+    //     name = name.replace(regex,"")
+        
+    //   })
+    //   console.log(name)
+    //   return [splitname,name]
+    // }
+
+
+  
+
+  
+    const result = ['Friend','Lover','Affectionate','Marriage','Enemy','Sibling']
+    //console.log(result)
+
+    const splitnames = splitArray(yourName,hisherName) 
+    console.log(splitnames);
+    
+
+    // const yourRemainder = getRemainder(splitnames,yourName)
+    // console.log(yourRemainder)
+
+    //console.log(splitnames[1]);
+    
+  
+
+
+
+
+
+
+
+
+  return (
+    <>
+        <Design></Design>
+        <label>Enter your name:</label>
+        <input  className = "yourname" onChange={event => setYourName(event.target.value)} />
+        <label>Enter his/her Name</label>
+        <input className = "hishername" onChange = {event => sethisherName(event.target.value) }/>
+        <button className = "calculate">Calculate</button>
+        <button className = "reset">Reset</button>   
+        <div className ="resultdiv"></div>
+      </>
+  )
+  
+    }
+
+
+export default App
---
+++ src/components/Design.js
@@ -0,0 +1,13 @@
+import {Wrapper} from '../styles/Design'
+const Design = () => {
+    return (
+        <Wrapper>
+
+        
+        
+
+        </Wrapper>
+    )
+}
+
+export default Design
