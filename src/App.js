664d161ac56128830f9b7d0e0bb07cb6a8acba4a
LP-SAKTHI <reputation.3.0.1.1@gmail.com>
2021-12-06 07:22:34 +0530

    3rd commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
 import { useState } from "react/cjs/react.development"
-import Design from "./components/Design"
+import {Wrapper} from './styles/Design'
 
 
 function App() {
 
+  
+let x1,x2;
+  //const [values,showvalues] = useState()
   const [yourName,setYourName] = useState("")
   const [hisherName,sethisherName] = useState("")
 
+  
   // console.log(yourName)
   // console.log(hisherName)
 
+  
   const splitArray = (yourName,hisherName) => {
-    if(yourName === null || hisherName === null)
+    if(yourName === "" || hisherName === "")
     {
       console.log("Enter Both Names!")
+      
     }
     else{
-      const yourNameSplit = yourName.split("")
-      const hisherNameSplit = hisherName.split("")
+      
+      x1 = yourName.split('.').join("").replace(/ /g,'')
+      console.log(x1)
+      x2 = hisherName.split('.').join("").replace(/ /g,'')
+      console.log(x2);
+      const yourNameSplit = x1.split("")
+      const hisherNameSplit = x2.split("")
       return [yourNameSplit,hisherNameSplit]
       
     }
@@ -28,6 +39,7 @@ function App() {
 
       splitname.map(item =>{
         const regex = new RegExp(`${item}`,"i")
+        
         name = name.replace(regex,"")
         
       })
@@ -41,71 +53,83 @@ function App() {
     const totalwords = yourRemainder + hisherRemainder;
     // console.log(totalwords);
     const totalCount = totalwords.length;
+    return totalCount
     //console.log(totalCount)
   }
   
-  const getResult = (result,setCount) =>
+  const getResult = (result,setCount,resultPart) =>
   {
-    if(yourName === true && hisherName === true)
-    {
+    
       for(let i = 6 ; i > 1 ; i--)
     {
-      result.splice((setCount)%i,1)
-      console.log(result)
-      console.log((setCount),result.length)
+      result.splice((setCount-1)%i,1)
+      //console.log(result)
+      // console.log((setCount),result.length)
 
-      if((setCount-1)%i < result.length || (setCount !== 0) )
+      if((setCount-1)%i < result.length-1 || (setCount-1 !== 0) )
       {
-        let resultPart = result.splice((setCount)%i,)
+        resultPart = result.splice((setCount-1)%i,)
         result = [...resultPart,...result]
         console.log(result)
+        
+        
       }
+      
     }
-    }
-    
-  }
-
   
+    
+    return [result]
+    }
+
+
+
+    
+  
+  const flames = () =>
+  {
     const result = ['Friend','Lover','Affectionate','Marriage','Enemy','Sibling']
     //console.log(result)
-
-    const splitnames = splitArray(yourName,hisherName) 
-    //console.log(splitnames);
     
+    if(yourName === "" || hisherName === ""){
+      document.querySelector(".resultdiv").innerHTML = "Enter both names!....";
+    }
+    
+    else if(yourName !== "" || hisherName !== "")
+    {
+    //console.log(splitnames);
+    const splitnames = splitArray(yourName,hisherName) 
 
-    const yourRemainder = getRemainder(splitnames[1],yourName)
-    const hisherRemainder = getRemainder(splitnames[0],hisherName)
+    const yourRemainder = getRemainder(splitnames[1],x1)
+    const hisherRemainder = getRemainder(splitnames[0],x2)
     // console.log(yourRemainder)
     // console.log(hisherRemainder)
     const setCount = getCount(yourRemainder[1],hisherRemainder[1])
     const setResult = getResult(result,setCount)
+    document.querySelector(".resultdiv").innerHTML = `${setResult}`;
+    }
+    
+    //console.log(setResult)
         // console.log(setResult)
 
     //console.log(splitnames[1]);
     
-  
-
-
-
-
-
-
-
+  }
 
   return (
-    <>
-        <Design></Design>
-        <label>Enter your name:</label>
-        <input  className = "yourname" onChange={event => setYourName(event.target.value)} />
-        <label>Enter his/her Name</label>
-        <input className = "hishername" onChange = {event => sethisherName(event.target.value) }/>
-        <button className = "calculate">Calculate</button>
-        <button className = "reset">Reset</button>   
+    <Wrapper>
+      <h1>♥ FLAMES ♥</h1>
+        <label className = "yourNameLabel">Enter your name:</label>
+        <input className = "yourname" onChange ={event => setYourName(event.target.value)} required/>
+
+        <label className = "hisherLabel">Enter his/her Name</label>
+        <input className = "hishername" onChange = {event => sethisherName(event.target.value) } required/>
+        <button className = "calculate" onClick = {()=>{flames()}}>Calculate</button>
+        <button className = "reset" onClick = {()=>{window.location.reload()}}>Reset</button>   
         <div className ="resultdiv"></div>
-      </>
+      </Wrapper>
   )
   
     }
 
-
+  
 export default App
--- src/styles/Design.js
+++ src/styles/Design.js
@@ -1,5 +1,24 @@
 import styled from 'styled-components'
 
 export const Wrapper = styled.div `
-    
+    //background-image: url(https://lh3.googleusercontent.com/proxy/vWHzYiK2JDgTRPdLjdFO75QC-nP3dsSZHOfDWHDDMYTF0hK1xijzXMlexDoMHu2-l8-ntd5xDo0Wsb2HWy6owRMAuJEySPCGyvNgoH0c-Y9g8KYQI7j-gJSbUKpp2H_styWq4uFtIqCE);
+    display:flex;
+    align-items:center;
+    justify-content: center;
+    flex-direction:column;
+    padding:5rem;
+
+
+    .yourname , .hishername{
+        border-radius: 40px;
+        height: 40px;
+        border:1px solid #000;
+    }
+    .calculate{
+        padding:20px;
+        border-radius:4rem;
+        border:1px solid #000;
+        cursor:pointer;
+
+    }
 `
